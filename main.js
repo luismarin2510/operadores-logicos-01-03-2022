790aed7351f1efcd5b1f3165bb76e703b964d2c6
luismarin2510 <opraedu@.com>
2022-03-03 19:21:09 -0500

    actualizacion de la clase

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
+let a = false;
+let b = false;
 let validar = function(id, ope){
     document.querySelector(id).insertAdjacentHTML("beforeend",
-`
-    <tr>
-        <td>${a}</td>
-        <td>${b}</td>
-        <td>${
-            (ope == "&") ? a && b 
-            : (ope == "|") ? a || b 
-            : ""
-         }</td>
-    </tr>
-`
+    `
+        <tr>
+            <td>${a}</td>
+            ${(ope != "!") ? `<td>${b}</td>` : ""}
+            <td>${
+                (ope == "&") ? a && b 
+                : (ope == "|") ? a || b                 
+                : (ope == "!") ? !a
+                : ""
+            }</td>
+        </tr>
+        <tr>
+            ${(ope != "!") ? `<td>${a}</td><td>${!b}</td>` : `<td>${!a}</td>`}
+            <td>${
+                (ope == "&") ? a && !b 
+                : (ope == "|") ? a || !b                 
+                : (ope == "!") ? !!a
+                : ""
+            }</td>
+        </tr>
+        <tr>
+            ${(ope != "!") ? `<td>${!a}</td><td>${b}</td>` : ""}
+            ${
+                (ope == "&") ? `<td>${!a && b}</td>` 
+                : (ope == "|") ? `<td>${!a || b}</td>`                 
+                : ""
+            }
+        </tr>
+        <tr>
+            ${(ope != "!") ? `<td>${!a}</td><td>${!b}</td>` : ""}
+            ${
+                (ope == "&") ? `<td>${!a && !b}</td>` 
+                : (ope == "|") ? `<td>${!a || !b}</td>`                 
+                : ""
+            }
+        </tr>
+    `
     );
 };
-validar("#Operador AND", "&");
-validar("#Operador OR", "|");
-
-console.log(!false);
+validar("#OperadorAND", "&");
+validar("#OperadorOR", "|");
+validar("#OperadorNOT", "!");
